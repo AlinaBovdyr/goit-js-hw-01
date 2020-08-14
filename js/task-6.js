3f780fe8efd162ea74f5ebd8a8e6a78501547a78
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-08-14 23:24:28 +0300

    Добавляет проверку на валидацию в задании 6

--- js/task-6.js
+++ js/task-6.js
@@ -4,12 +4,15 @@ let message;
 
 do {
   input = prompt('Введите число');
-  total += Number(input);
-  if (input !== null) {
-    console.log(input);
-  }
-} while (input !== null);
-console.log('Общая сумма чисел равна', total);
 
+  if (isNaN(input)) {
+    alert('Было введено не число, попробуйте еще раз');
+    total = 0;
+  } else if (Number(input)) {
+    total += Number(input);
+  }
+  console.log(input);
+} while (input !== null);
+console.log(`Общая сумма чисел равна ${total}`);
 message = `Общая сумма чисел равна ${total}`;
 alert(message);
